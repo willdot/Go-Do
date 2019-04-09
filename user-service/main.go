3774a0cf13e8c4d323c78b3fd0bced0903da8950
Will <will7989@hotmail.com>
2019-04-09 11:32:12 +0100

    forgot to run server

--- user-service/main.go
+++ user-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	pb "go-do/user-service/proto/auth"
 
 	"github.com/micro/go-micro"
@@ -15,4 +16,9 @@ func main() {
 	srv.Init()
 
 	pb.RegisterAuthHandler(srv.Server(), &userHandler{})
+
+	// Run the server
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
